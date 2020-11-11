76e7d13d550110db7730fc38dccb3c009a109a6d
William Anderson <willjatlas@googlemail.com>
2020-11-11 21:33:03 +0000

    Comppleted first task

--- src/compare.py
+++ src/compare.py
@@ -1,2 +1,7 @@
-def compare():
-    pass
+def compare(num_1,num_2):
+    if num_1 == num_2:
+        return f"{num_1} is the same as {num_2}"
+    if num_1 < num_2:
+        return f"{num_1} is less than {num_2}"
+    else:
+        return f"{num_1} is greater than {num_2}"
--- tests/compare_test.py
+++ tests/compare_test.py
@@ -6,3 +6,11 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_5_10_returns_5_is_less_then_10(self):
+        self.assertEqual("5 is less than 10", compare(5, 10))
+
+    def test_compare_10_10_returns_10_is_the_same_as_10(self):
+        self.assertEqual("10 is the same as 10", compare(10, 10))
+
+    
