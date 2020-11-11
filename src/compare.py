1ab5e057c6b32cbe3e2140fc923184204e1f611d
William Anderson <willjatlas@googlemail.com>
2020-11-11 21:39:25 +0000

    Final changes complete

--- src/compare.py
+++ src/compare.py
@@ -1,7 +1,7 @@
-def compare(num_1,num_2):
-    if num_1 == num_2:
-        return f"{num_1} is the same as {num_2}"
-    if num_1 < num_2:
-        return f"{num_1} is less than {num_2}"
+def compare(number_1,number_2):
+    if number_1 == number_2:
+        return f"{number_1} is the same as {number_2}"
+    elif number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
     else:
-        return f"{num_1} is greater than {num_2}"
+        return f"{number_1} is greater than {number_2}"
